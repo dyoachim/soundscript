6fe73abbc24a2ac41dfbcb2f83ac47938145a523
MonicaDev <monicacho21@gmail.com>
2014-07-03 15:52:39 -0500

    completed routes for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+include SessionsHelper
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to root_path
+		else
+			@error = "Invalid shit.  Try again."
+			render 'new'
+		end
+	end
+
+	def edit
+		if current_user == User.find(params[:id])
+			@user = current_user
+			render 'edit'
+		else
+			not_found
+		end
+
+	end
+
+	def show
+		if current_user == User.find(params[:id])
+			@user = current_user
+			render 'show'
+		else
+			not_found
+		end
+	end
+
+	def update
+		if current_user == User.find(params[:id])
+			current_user.update(user_params)
+			redirect_to user_path(current_user)
+		else
+			not_found
+		end
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		session.clear
+		redirect_to root_path
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:username, :email, :password, :password_confirmation)
+	end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140703144944) do
 
   # These are extensions that must be enabled in order to support this database
@@ -35,16 +34,17 @@ ActiveRecord::Schema.define(version: 20140703144944) do
   create_table "tracks", force: true do |t|
     t.integer  "user_id"
     t.string   "youtube_url"
+    t.string   "primary_language"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
     t.string   "username"
-    t.string   "oauth_token"
-    t.string   "oauth_secret"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-
+end
