a8be7c9259952333e71f353221a0005ceca032f9
MonicaDev <monicacho21@gmail.com>
2014-07-06 19:16:00 -0500

    submit button added and refresh on submit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,8 +23,13 @@ $(document).ready( function(){
     $('#browse').hide();
   });
 
+  $('.track_divs').on("click", ".timeButton", function(){
+  	alert('You have successfully submitted your track.');
+  	location.reload();
+  });
+
   $('.appendTrack').on("click",function(){
-    $('.track_divs').append("<div class='language_box'>English</div><div class='tracks_box' style='width:"+ parseInt($('#totalDuration').text()) * 10 + "em;'><button class='timeButton'>Get timestamps</button></div>");
+    $('.track_divs').append("<div class='language_box'>English</div><div class='tracks_box' style='width:"+ parseInt($('#totalDuration').text()) * 10 + "em;'><button class='timeButton'>Submit</button><button class ='deleteButton'>Delete</button><button class ='deleteEdit'>Edit</button></div>");
 
     $('#play').on("click", function(){
       $('#play').replaceWith( "<a href='javascript:ytplayer.pauseVideo()'><div id='pause'></div></a> ");
@@ -44,14 +49,36 @@ $(document).ready( function(){
     var data = { data: postIts }
     $.post(url, data, function( response ) {});
   });
-
+  $('.track_divs').on("click", ".deleteButton", function(){
+  });
   $('.track_divs').on('dblclick', ".tracks_box", function(event){
     if (event.target.className == "tracks_box") {
       $(this).append( buildPostIt(event) );
     }
-    $('.post-it').draggable({ handle: ".header", containment: "parent" });
+    $('.post-it').draggable({ handle: ".header", containment: "parent" }).resizable();
     $('.remove_note').on('click', function(){
       $(this).parent().parent().remove();
     });
   }); 
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
