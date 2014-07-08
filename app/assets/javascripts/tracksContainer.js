0cd8728a5b451e98d86d0b8b1bff760c5ba7ea94
MonicaDev <monicacho21@gmail.com>
2014-07-08 16:07:26 -0500

    added a non functional dropdown languages box. moved submit into the language box div

--- app/assets/javascripts/tracksContainer.js
+++ app/assets/javascripts/tracksContainer.js
@@ -1,19 +1,36 @@
-function TracksContainer(duration, loggedIn, tracks, userId) {
+function TracksContainer(duration, loggedIn, tracks, userId, languages) {
 	this.duration = duration;
 	this.loggedIn = loggedIn;
 	this.tracks = tracks;
 	this.trackNum = tracks.length;
 	this.userId = userId;
+	this.languages = languages;
 
 	this.initialize();
 }
 
 TracksContainer.prototype.constructElement = function() {
-	var track = "<div class='language_box'></div><div class='tracks_box' style='width:"+ this.duration * 10 + "em;'>"
+console.log(languages);
+// console.log(languages[0]['title']);
 
-  if (this.loggedIn) {
-  	track += "<button class='timeButton'>Submit</button>"
-  }
+
+// var wanted = languages.filter( function(language){return (language.title== "English");} );
+
+// console.log(wanted);
+
+	var track = ""
+	track += "<div class='language_box'>";
+	track += "<select name = 'Language'>";
+
+		for(var i = 0; i < this.languages.length; i++){
+			track += "<option>" + this.languages[i]['title'] + "</option>";
+		}
+			track += "</select>";
+			if (this.loggedIn) {
+		  	track += "<button class='timeButton'>Submit</button>"
+		  }
+	track += "</div><div class='tracks_box' style='width:"+ this.duration * 10 + "em;'>";
+
   track += "<div class='progressBar'></div></div>";
 	$('.track_divs').html(track);
 };
@@ -31,9 +48,11 @@ TracksContainer.prototype.showExistingTranscript = function() {
 		for(var i = 0; i < this.trackNum; i++) {	
 
 			transcript += "<div class='language_box'>";
+
     	if (true){
+
     		transcript += "<form action='/videos/" + this.tracks[i]['videoId'] + "/tracks/" + this.tracks[i]['trackId'] + "' method='POST'>";
-   
+
 
     		transcript += "<input name='authenticity_token' type='hidden' value ='" + AUTH_TOKEN +"'/>";
     		transcript += "<input name='_method' type='hidden' value='delete' /><input type='submit' class ='deleteButton' value='Delete'></form>";
